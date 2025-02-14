fc702c97fd227ea8a6a06ab804c97a2202accd69
Rabin-web-dev <rabin.pvtltd@gmail.com>
2025-02-14 08:10:46 +0530

    Add some DB

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,18 @@
-const connectMongoDB = require(" ./config/database.js ");
+require("dotenv").config();
+const connectMongoDB = require("./config/Database.js");
 const express = require("express");
 
+
 const app = express();
+
+//Middleware
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.send("Server is running!");
+});
 
 connectMongoDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is successfully running on port ${PORT}`));
-
